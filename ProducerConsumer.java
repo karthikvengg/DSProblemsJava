4a662a80726898312bb122c50d01cba23e4af71d
Karthik <karthikv.engg@gmail.com>
2017-01-25 00:42:29 +0530

    Producer Cosumer using Wait and Notify methods

--- ProducerConsumer.java
+++ ProducerConsumer.java
@@ -1,83 +1,81 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Producer Consumer using Notify and Wait methods
  */
 package producerconsumer;
-import java.util.*;
+import java.util.Random;
+import static java.lang.System.out;
+import java.util.Stack;
+
 /**
  *
  * @author Karthikeyan_Varadara
  */
 public class ProducerConsumer {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Main m = new Main();
-        Producer t1 = new Producer(m);
-        Consumer t2 = new Consumer(m);
-        t1.start();
-        t2.start();
-    }    
-}
-
-class Main{
-    Stack st=new Stack();
-    boolean flag = true;
-    public synchronized void put(int a){
-        if(flag==true){
-            st.add(a);
-            System.out.println("Produced: "+a);
-            flag=false;
-        }        
-    }
     
-    public synchronized void get(){
-        if(flag==false){                    
-            Integer b = (Integer)st.pop();
-            flag=true;
-            System.out.println("Consumer: "+b);                 
-        }        
+    public static void main(String[] args) {
+        ProducerConsumerDemo pc = new ProducerConsumerDemo();
+        
+        new Thread("One"){
+            public void run(){
+                for(int i=0;i<10;i++){
+                    try {
+                        pc.produce();
+                        Thread.sleep(200);
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        }.start();
+        
+        new Thread("Two"){
+            public void run(){
+                for(int i=0;i<10;i++){
+                    try {
+                        pc.consume();
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        }.start();
+        
     }   
 }
-class Producer extends Thread{    
-    Main m;
-    Producer(Main n){
-        m=n;
-    }
-    @Override
-    public void run(){
-        for(int i=1;i<=10;i++){            
-            m.put(i);
+
+class ProducerConsumerDemo{
+    Random r = new Random();
+    int n;
+    Stack st = new Stack();
+    synchronized void produce(){
+        if(st.size()==5){
+            out.println("Waiting to be consumed..");
             try{
-                Thread.sleep(1000);    
-            }   
-            catch(InterruptedException e){
-                System.out.println(e);
-            }
+                this.wait();
+            }                
+            catch(InterruptedException ex){
+                
+            }          
         }
+        n = r.nextInt(100);
+        st.push(n);
+        out.println("Produced: "+n);
+        this.notify();
+    }
+    synchronized void consume(){
+        if(st.size()==0){
+            out.println("Waiting for the producer to produce..");
+            try{
+                this.wait();
+            }                
+            catch(InterruptedException ex){
+                
+            }           
+        }        
+        out.println("Consumed: "+st.pop());
+        this.notify();        
     }
 }
 
 
-class Consumer extends Thread{
-    Main m;
-    Consumer(Main n){
-        m=n;
-    }
-    @Override
-    public void run(){
-        for(int i=1;i<=10;i++){
-            m.get();
-            try{
-                Thread.sleep(1000);    
-            }   
-            catch(InterruptedException e){
-                System.out.println(e);
-            }            
-        }
-    }    
-}
