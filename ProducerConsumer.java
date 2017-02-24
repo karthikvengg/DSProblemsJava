595308be27ff1a27e765ec548010693fc2167e22
Karthik <karthikv.engg@gmail.com>
2017-02-24 21:41:32 +0530

    Producer Consumer Problem using Blocking Queue

--- ProducerConsumer.java
+++ ProducerConsumer.java
@@ -1,81 +1,71 @@
 /*
-Producer Consumer using Notify and Wait methods
+Producer Consumer Problem with Blocking Queue
  */
-package producerconsumer;
-import java.util.Random;
-import static java.lang.System.out;
-import java.util.Stack;
+package ProducerConsumer;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author Karthikeyan_Varadara
- */
 public class ProducerConsumer {
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        ProducerConsumerDemo pc = new ProducerConsumerDemo();
         
-        new Thread("One"){
+        BlockingQueue queue = new ArrayBlockingQueue(5);
+        Demo d = new Demo(queue);
+        new Thread("Producer"){
             public void run(){
-                for(int i=0;i<10;i++){
+                for(int i=0;i<5;i++){
                     try {
-                        pc.produce();
-                        Thread.sleep(200);
-                    } catch (InterruptedException ex) {
-                        
-                    }
-                }
-            }
-        }.start();
-        
-        new Thread("Two"){
-            public void run(){
-                for(int i=0;i<10;i++){
-                    try {
-                        pc.consume();
+                        d.produce(i);
                         Thread.sleep(1000);
                     } catch (InterruptedException ex) {
-                        
+                        Logger.getLogger(BubbleSort.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
             }
         }.start();
-        
-    }   
+        new Thread("Producer"){
+            public void run(){
+                for(int i=0;i<5;i++){
+                    try {
+                        d.consume();
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(BubbleSort.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }.start();
+    }
+    
 }
 
-class ProducerConsumerDemo{
-    Random r = new Random();
-    int n;
-    Stack st = new Stack();
-    synchronized void produce(){
-        if(st.size()==5){
-            out.println("Waiting to be consumed..");
-            try{
-                this.wait();
-            }                
-            catch(InterruptedException ex){
-                
-            }          
+class Demo{
+    BlockingQueue queue;
+    
+    Demo(BlockingQueue queue){
+        this.queue=queue;
+    }
+    void produce(int i){
+        try {
+            queue.put(i);
+            System.out.println("Produced: "+ i);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Demo.class.getName()).log(Level.SEVERE, null, ex);
         }
-        n = r.nextInt(100);
-        st.push(n);
-        out.println("Produced: "+n);
-        this.notify();
     }
-    synchronized void consume(){
-        if(st.size()==0){
-            out.println("Waiting for the producer to produce..");
-            try{
-                this.wait();
-            }                
-            catch(InterruptedException ex){
-                
-            }           
-        }        
-        out.println("Consumed: "+st.pop());
-        this.notify();        
+    
+    void consume(){
+        int j;
+        try {
+            j=(Integer)queue.take();
+            System.out.println("Consumed: "+j);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Demo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
-
-
