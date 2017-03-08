ae583737afc416ba1df7db4b95a842630a42aedf
Karthik <karthikv.engg@gmail.com>
2017-03-08 22:21:35 +0530

    n^3 solution

--- CommonElements.java
+++ CommonElements.java
@@ -1,5 +1,5 @@
 
-/* Print the common elements in 3 arrays. Inefficient solution. O(n^4)*/
+/* Print the common elements in 3 arrays. Inefficient solution. O(n^3)*/
 
 import java.io.*;
 import java.util.*;
@@ -8,25 +8,17 @@ class myCode
 {
     public static void main (String[] args) throws java.lang.Exception
     {
-        int[] a={1,2,3,4,5};
-        int[] b={4,7,9,10};
-        int[] c={2,6,8,9};
+		int[] a={1,2,3,4,5};
+        int[] b={4,3,9,10};
+        int[] c={2,3,8,4};
         
         List<Integer> ls=new ArrayList<Integer>();
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<b.length;j++){
-                if (a[i]==b[j]){
-                    ls.add(a[i]);
-                }
-            }
-        }
-        for(int k=0;k<c.length;k++){     
-            for(int f:ls){
-                if(c[k]==f){
-                    System.out.println(f);
-                }                
-            }            
-        }
+        for(int i=0;i<a.length;i++)
+            for(int j=0;j<b.length;j++)
+                for(int k=0;k<c.length;k++)
+                    if (a[i]==b[j])
+                        if (b[j]==c[k])
+                            System.out.println(c[k]);                                        
     }
 }
  
