83c9319d62b7a29543eb1db465158e1051a770a2
karthikvengg <karthikv.engg@gmail.com>
2017-09-13 11:55:14 +0530

    JMX

---
+++ JMX Sample/SystemConfigMBean.java
@@ -0,0 +1,10 @@
+public interface SystemConfigMBean {
+
+    public void setThreadCount(int noOfThreads);
+    public int getThreadCount();
+    
+    public void setSchemaName(String schemaName);
+    public String getSchemaName();
+    
+    public String doConfig();
+}
